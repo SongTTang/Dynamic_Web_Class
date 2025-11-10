cc0cefe0010e5b2d4f805dcd8343d1288d6ab76b
SongTTang <126841358+SongTTang@users.noreply.github.com>
2025-11-10 02:35:10 -0500

    add homework todo-v4

--- Week10/todo-list-v4/src/App.js
+++ Week10/todo-list-v4/src/App.js
@@ -1,12 +1,7 @@
-import { useState, useContext } from 'react'
-import TodoContext from './context/todos'
 import TodoCreate from './components/TodoCreate'
 import TodoList from './components/TodoList'
-import { Provider } from './context/todos'
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-
   return (
     <div>
       <TodoCreate />
